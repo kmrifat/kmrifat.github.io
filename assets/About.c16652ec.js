2c4240b91ab9a2417ae3e6e079ca96cfd27e9c7c
K M Rifat ul alom <kmrifat@gmail.com>
2022-10-01 23:56:07 +0600

    deploy

---
+++ assets/About.c16652ec.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index.ddfc7c53.js";const o={name:"About"};function e(r,n,s,a,c,_){return" hello about "}const u=t(o,[["render",e]]);export{u as default};
--- assets/Home.6b84090f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as t}from"./index.0b97d9b9.js";const c={name:"Home"};function n(r,s,a,_,l,m){return o(),t("h1",null,"Hello Home")}const f=e(c,[["render",n]]);export{f as default};
